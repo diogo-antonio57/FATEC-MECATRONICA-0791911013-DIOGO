d0aa3466956f3af5464527f16238cc85593e6ce3
diogo-antonio57 <69904641+diogo-antonio57@users.noreply.github.com>
2020-11-19 11:04:22 -0300

    Create programa01.ino

---
+++ Microcontroladores-2020-2/Pratica06/programa01.ino
@@ -0,0 +1,53 @@
+const byte SERVO = 5 ;
+const byte MOTOR_DC = 3 ;
+const byte POTENCIOMETRO = 0 ;
+const byte LDR = 1 ;
+byte const CHAVE_1 = 2 ;
+byte const CHAVE_2 = 6 ;
+byte const CHAVE_3 = 7 ;
+const byte CHAVES [] = {CHAVE_1, CHAVE_2, CHAVE_3};
+LED1 de byte const = 12 ;
+LED2 de byte const = 11 ;
+const byte LED3 = 10 ;
+LED de byte const 4 = 9 ;
+LED 5 de byte const = 8 ;
+LEDS de byte const [] = {LED1, LED2, LED3, LED4, LED5};
+const byte LED_LIGADO = 0 ;
+const byte LED_DESLIGADO = 1 ;
+const byte CHAVE_ACIONADA = 0 ;
+const byte CHAVE_DESACIONADA = 1 ;
+const byte MOTOR_DC_LIGADO = 1 ;
+byte const MOTOR_DC_DESLIGADO = 0 ;
+
+// Função para inicializar o hardware
+void  inicializar_hardware () {
+  pinMode (SERVO, OUTPUT);
+  pinMode (MOTOR_DC, OUTPUT);
+  para ( int i = 0 ; i < 5 ; i ++) {
+    pinMode (LEDS [i], OUTPUT);
+    digitalWrite (LEDS [i], LED_DESLIGADO);
+  }
+  para ( int i = 0 ; i < 3 ; i ++)
+    pinMode (CHAVES [i], INPUT_PULLUP);
+  // Apenas porque estamos utilizando um pullup externo
+  pinMode (CHAVE_1, INPUT);
+}
+
+void  realizarLeituraPotenciometro () {
+  // Realiza a leitura
+  int dado = analogRead (POTENCIOMETRO);
+  // Imprime ele na porta
+  Serial. println (dado);
+}
+
+void  setup () {
+  inicializar_hardware ();
+  // configurar a comunicação Serial
+  Serial. começar ( 9600 );
+}
+
+void  loop () {
+  // CHAVE_1 - Realizar leitura analógica
+  if ( digitalRead (CHAVE_1) == CHAVE_ACIONADA)
+    realizarLeituraPotenciômetro ();
+}
