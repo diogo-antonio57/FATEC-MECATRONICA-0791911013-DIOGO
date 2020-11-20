eb519b577d1f1924855d0d425e9911322413909a
diogo-antonio57 <69904641+diogo-antonio57@users.noreply.github.com>
2020-11-20 04:04:25 -0300

    Create programa05.c

---
+++ Microcontroladores-2020-2/Pratica07/programa05.c
@@ -0,0 +1,12 @@
+# inclui  < stdio.h >
+# inclui  < string.h >
+int  main ( void ) {
+  char msg [ 20 ];
+
+  int temperatura_calculada = 2345 ;
+
+  sprintf (msg, " % i C " , temperatura_calculada);
+
+  printf ( " % s \ n " , msg);
+  return  0 ;
+}
