7b072390c991f0fc0512972b6c2f58c4ed9dd0f1
diogo-antonio57 <69904641+diogo-antonio57@users.noreply.github.com>
2020-11-20 04:06:22 -0300

    Create programa07.c

---
+++ Microcontroladores-2020-2/Pratica07/programa07.c
@@ -0,0 +1,30 @@
+# inclui  < stdio.h >
+# inclui  < string.h >
+int  main ( void ) {
+  char msg [ 20 ];
+
+  // Inicializa uma msg como uma string vazia
+  msg [ 0 ] = ' \ 0 ' ;
+  char dado;
+  faça {
+    // Ler uma letra do teclado
+    dado = getchar ();
+    printf ( " Digitado: % c \ n " , dado);
+    // Tudo que for digitado que não for enter (\ n) vai para dentro da variavel msg
+    if (dado! = ' \ n ' ) {
+      sprintf (msg, " % s% c " , msg, dado);
+    }
+    printf ( " Valor de msg: % s \ n " , msg);
+
+  } enquanto (dado! = ' q ' );
+  printf ( " FIM DIGITACAO! \ n " );
+  printf ( " % s \ n " , msg);
+
+  // Verifica uma mensagem recebida
+  if ( strcmp (msg, " olaq " ) == 0 ) {
+    printf ( " Msg valida! \ n " );
+  } else {
+    printf ( " Msg invalida recebida! \ n " );
+  }
+  return  0 ;
+}
